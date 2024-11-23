f2fa652fd2224fd3cd25fcdc59de440405963fe8
Silla K <79395251+silla2807@users.noreply.github.com>
2024-11-23 13:42:25 +0530

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Silla text', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Silla/i); // Case-insensitive match for "Silla"
-  expect(textElement).toBeInTheDocument();
-});
